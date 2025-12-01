6fdd0b1f8be5727fa7e222673bf6d1ab8a34dd70
Ben Weston <westonb26918@proton.me>
2025-12-01 14:22:46 +0000

    Clean up repo & make minor updates

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
-resource "digitalocean_project" "sandbox" {
-  name        = "Sandbox"
-  description = "Non-production sandbox for service exploration, prototypes, and demos."
+resource "digitalocean_project" "sandbox_infra" {
+  name        = "Sandbox Infra"
+  description = "Infrastructure as code for creating a disposable sandbox environment for testing, experimentation, and learning."
   purpose     = "Operational / Developer tooling"
   environment = "Development"
 }
